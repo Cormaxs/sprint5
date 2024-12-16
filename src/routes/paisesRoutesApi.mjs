5d6064f57512b8860210bc298fab44f6452b1a74
cormaxs <tomasbarros076@gmail.com>
2024-12-16 01:03:36 +0100

    modificado

---
+++ src/routes/paisesRoutesApi.mjs
@@ -0,0 +1,25 @@
+import express from 'express';
+import { cargarPaises, paisesEspanol, addPais , editarPaises,  renderPaisEditar, eliminarPais} from '../controllers/paisesApiController.mjs';
+
+
+const routerApi = express.Router();
+
+//cargar todos los países desde la API
+routerApi.get('/cargar-paises', cargarPaises); 
+//filtrar los resultados en paises que hablen español
+routerApi.get('/paises-espaniol', paisesEspanol);
+//agregar un nuevo país
+routerApi.get('/agregar-pais', (req, res)=>{
+    res.render('agregarPais') 
+});
+routerApi.post('/agregar-pais', addPais);
+
+//cargar el formulario de edición
+routerApi.get('/editar-pais/:id', renderPaisEditar);
+//editar un país formulario
+routerApi.post('/editar-pais/:id', editarPaises);
+//elimina el pais
+routerApi.delete('/eliminar-pais/:id', eliminarPais);
+
+
+export default routerApi;
